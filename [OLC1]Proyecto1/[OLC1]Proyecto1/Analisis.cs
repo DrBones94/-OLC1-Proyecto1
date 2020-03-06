ae35fe1a1cd1052cab2c0c576f528d93bcdafa79
DrBones94 <danilo.gpa94@gmail.com>
2020-03-06 03:21:08 -0600

    Prueba1
    Inicio de proyecto

---
+++ [OLC1]Proyecto1/[OLC1]Proyecto1/Analisis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace _OLC1_Proyecto1
+{
+    class Analisis
+    {
+        static List<Token> LTokens = new List<Token>(); //Lista de tkens a enviar en el Analizador Sintactico.
+
+    \
+    }
+}
---
+++ [OLC1]Proyecto1/[OLC1]Proyecto1/Token.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _OLC1_Proyecto1
+{
+    class Token
+    {
+        public enum Type
+        {
+            id, //Identificador
+            flecha, //Asignacion ->
+            puntoC, //Punto y coma ;
+            dosP, //Dos Puntos :
+            punto, //Punto .
+            or, //OR |
+            interrogacionC, //Interrogacion Cerrado ?
+            asterisco, //Asterisco *
+            mas, //Mas +
+            separador, //Separador ~
+            coma, //Coma ,
+            prConj, //Palabra reservada CONJ
+            caracter, //Caracter
+            numero, //Numero
+            comentario //Comentario
+        }
+
+        private String lexema;
+
+        public String Lexema
+        {
+            get { return lexema; }
+            set { lexema = value; }
+        }
+        private Type id;
+
+        internal Type Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        private String token;
+
+        public String Token1
+        {
+            get { return token; }
+            set { token = value; }
+        }
+        private int fila;
+
+        public int Fila
+        {
+            get { return fila; }
+            set { fila = value; }
+        }
+        private int columna;
+
+        public int Columna
+        {
+            get { return columna; }
+            set { columna = value; }
+        }
+
+        public Token(String lexema, Type id, String token, int fila, int columna)
+        {
+            this.lexema = lexema;
+            this.id = id;
+            this.token = token;
+            this.fila = fila;
+            this.columna = columna;
+        }
+
+
+    }
+}
